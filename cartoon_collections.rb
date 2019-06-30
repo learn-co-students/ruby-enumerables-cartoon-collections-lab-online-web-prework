3e2fc7ae9d201fb55586e78fe416510fe9f429c3
Austin Jacks <austinjacksforex@gmail.com>
2019-06-30 20:48:52 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,18 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{|item, index| puts "#{index +1} #{item}"  }
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+ array.map{|element| element.capitalize + "!"}
+ end 
+
+
+def long_planeteer_calls(array)
+ array.include?(array.length > 4)
+ array.any? {|word| word.length > 4}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
-end
-
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.find{|i| cheese_types.include?(i)}
 end
