06058d0ff2c4201fcb6366e6af0a0582449c8a3a
Sydnee Sampson <sbsydnee161416@gmail.com>
2019-07-09 17:52:41 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,6 @@
 def roll_call_dwarves(array)
- order = 1
- array.each do |name|
-  puts "#{order}, #{name}" 
-  order += 1
- end 
+ array.each_with_index { |name, index|
+  puts "#{index +1}, #{name}" }
 end
 
 def summon_captain_planet(array)
