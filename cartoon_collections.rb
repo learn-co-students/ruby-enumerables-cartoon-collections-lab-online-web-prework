ea67393b90f92af036aaf2d9baa75d74e6bdcc0b
Ines Angelica Thompson <i.thompson89@outlook.com>
2019-07-13 02:18:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,23 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each_with_index{|name, i| puts "#{i + 1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(arr)
   # Your code here
+  return arr.map{|name| "#{name.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(arr)
   # Your code here
+  return arr.reduce(0) {|prev, i| prev += i.length > 4 ? 1 : 0} > 0
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(arr)
+  cheese = ["cheddar", "gouda", "camembert"]
+  arr.each{|i|
+    if cheese.include?(i)
+      return i
+    end
+  }
+  return nil
 end
