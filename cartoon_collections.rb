0a8d396f99f5b7a2e4cd8208b2692fb8809dc045
beaming-terminal-3309 <jonny_rawlin@live.co.uk>
2019-07-13 09:44:03 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,21 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  array.each.with_index(1) do |value, index|
+  puts "#{index}: #{value}"
+end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  new_array = array.each {|element| element.capitalize! }
+  final_array = new_array.each {|word| word.concat("!")}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+def long_planeteer_calls(array)
+  array.any? { |s| s.size > 4 }
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  a = array & cheese_types
+  a.shift
 end
