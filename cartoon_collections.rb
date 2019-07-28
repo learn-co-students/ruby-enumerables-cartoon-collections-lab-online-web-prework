17ad2d0e1afddc70fbf4984865e20bfcae5c915f
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-07-28 06:12:48 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,30 +1,15 @@
 def roll_call_dwarves(array)
-  i = 0 
-  while i < array.length 
-    puts "#{i + 1}. #{array[i]}"
-    i += 1 
-  end 
+  array.each.with_index(1) {|name, index|
+    puts "#{index}. #{name}"}
 end
 
 
   def summon_captain_planet(array)
-  new_array = []
-  i = 0 
-  while i < array.length 
-    new_array << array[i].capitalize + "!"
-    i += 1 
-  end 
-  new_array
+  array.collect {|name| name.capitalize + "!"} 
 end
 
 def long_planeteer_calls(array) 
-  i = 0 
-  if  array.any? {|i| i.length > 4}
-    return true
-  else 
-    return false
-  i = i + 1
-  end 
+ array.any? {|word| word.length > 4}
 end
 
 
