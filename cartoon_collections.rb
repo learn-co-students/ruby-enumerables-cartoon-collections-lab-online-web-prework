58ebfb947fd7700d926e99de07c0a815358454e0
Joey Luis Echevarria <jechevarria719@gmail.com>
2019-08-04 22:48:17 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,7 +3,7 @@
 describe "cartoon collection"
   describe "#roll_call_dwarves"
     it "prints out the 7 dwarfs in a numbered list"
-      dwarves = []
+      dwarves = ["dopey", ]
 
 
 def summon_captain_planet# code an argument here
