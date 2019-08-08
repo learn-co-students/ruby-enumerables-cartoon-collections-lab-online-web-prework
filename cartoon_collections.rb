f14957ed6ed158f41dc05d93f9a0df7bd2fe8def
Christina Gonzalez <cmgamway@gmail.com>
2019-08-08 20:11:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,24 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves
+  when "prints out the 7 dwarfs in a numbered list" do
+    roll_call_dwarves = ("Doc", "Dopey", "Bashful", "Grumpy")
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  when "capitalizes each element and adds an exclamation mark" do
+    planeteer_calls = ("earth", "wind", "fire", "water", "heart")
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  when"returns true if any calls are longer than 4 characters" do
+  when  "returns false if all calls are 4 characters or less" do
+    short_words = ("puff", "go", "two")
+    assorted_words = ("two", "go", "industrious", "bop")
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese
+  when "returns the first element of the array that is cheese" do
+  when "returns nil if the array does not contain a type of cheese" do
+    snacks = ["crackers", "gouda", "thyme"]
+    soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+    cheese_types = ["cheddar", "gouda", "camembert"]
 end
