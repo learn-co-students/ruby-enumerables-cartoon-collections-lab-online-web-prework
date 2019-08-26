76966694be409b9b1447b250435d5597795bfce1
munificent-commit-7390 <jaredjames2020@gmail.com>
2019-08-26 23:15:45 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,25 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarf)
+  dwarf_hash = Hash.new
+  dwarf.each_with_index { |dwarf, index| 
+    dwarf_hash[dwarf] = index
+    puts "#{index+1}.  #{dwarf}" 
+  } 
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+    planeteer_calls.collect {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+    if calls.any? {|x| x.length > 4}
+      p true
+    elsif calls.all? {|x| x.length <= 4}
+      p false
+    end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
+def find_the_cheese(cheese)
+  cheese.find do |x|
+    x == "cheddar" || x == "gouda" || x == "camembert"
+  end 
 end
