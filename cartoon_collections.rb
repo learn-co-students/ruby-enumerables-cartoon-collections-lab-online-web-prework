d86978cb4f2a1825df9d61f32b7930a4374084d7
Hannah Harrington <hannahharrington@outlook.com>
2019-09-01 05:18:24 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,12 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves = ["Dopey", "Grumpy", "Bashful", "Poopy", "Sloopy", "Thrifty", "Benny"]
+  (/1.*Dopey/)
+  (/2.*Grumpy/)
+  (/3.*Bashful/)
+  (/4.*Poopy/)
+  (/5.*Sloopy/)
+  (/6.*Thrifty/)
+  (/7.*Benny/)
+   p output = capture_stdout
 end
 
 def summon_captain_planet# code an argument here
