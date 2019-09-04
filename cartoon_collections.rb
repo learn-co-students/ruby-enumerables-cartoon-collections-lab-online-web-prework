2a1282dceda0910afed9f0dd660338b1b0276e27
Seyedpouya hajiseyedjavadi <pouya.javadi.resume@gmail.com>
2019-09-04 01:25:23 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,46 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)
+hash = Hash.new
+
+ 
+  dwarves.each_with_index {|item, index|
+  index += 1 
+  hash[index] = item
+  }
+  puts hash
+  
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+
+
+
+def summon_captain_planet(numbers)
+  numbers.map! {|num| num.capitalize + '!'}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+
+
+def long_planeteer_calls(array)
+  array.any? {|word_true| word_true.length  > 4 ? true : false}
 end
 
-def find_the_cheese# code an argument here
+
+
+
+def find_the_cheese(contains_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+ i = 0
+ while i < contains_cheese.length do
+   if cheese_types.include?contains_cheese[i]
+     return contains_cheese[i]
+   else
+     i += 1
+   end
+ end
+ return nil
 end
+
+
+   
