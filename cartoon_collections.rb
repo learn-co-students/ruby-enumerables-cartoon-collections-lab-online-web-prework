b7da91f3e7eed569a0be19e3ba6d8a2460953be4
Wendy Harris <wendyv510@hotmail.com>
2019-09-23 22:02:00 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,28 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves["Doc","Dopey","Bashful","Grumpy"]
+  do
+   (roll_call_dwarves (each_with_index))
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  do
+    .map(planeteer_calls{|n| n capitalize & !})
+    find_all(4 letters)
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls
+  short_words=["puff", "go", "two"]
+  long_planeteer_calls(short_words)
+  
+  assorted_words = ["two", "go", "industious", "bop"]
+  long_planeteer_calls(assorted_words)
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks=["crackers", "gouda", "thyme"]
+  find_the_cheese(snacks)
+  
+  soup= ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+  find_the_cheese(soup)
 end
