967154f8fc155c34fbdc1978806eadde3aaf6ab1
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 23:22:18 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(names)
+  
+  names.each_with_index do |name, index|
+    puts "#{index + 1}.*#{name}"
+  end
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  temp = []
+  
+  planeteer_calls.collect do |call|
+    temp << "#{call.capitalize}!"
+  end
+  
+  return temp
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  
+  planeteer_calls.collect do |call|
+    if call.length > 4
+      return true
+    end
+  end
+  
+  return false
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  cheese.collect do |item|
+    cheese_types.each do |type|
+      if item == type
+        return item
+      end
+    end
+  end
+  
+  return nil
 end
