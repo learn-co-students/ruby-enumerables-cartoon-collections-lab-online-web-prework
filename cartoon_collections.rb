5ce8945f8a45bbcd1b10c73411024167487b2299
Nethelbert <gilblasse@gmail.com>
2019-11-04 00:06:09 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,26 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+def roll_call_dwarves(dwarves)
+  dwarves.map.with_index {|name,i| p "#{i+1}. #{name}\n"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.collect {|item| "#{item.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  character_test = array.reduce([]) do |memo, word|
+                        if word.length > 4
+                          return true
+                        else
+                          memo << word
+                        end
+                    end
+  character_test.class == Array ? false : true
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  find = cheese_types.map {|item| item if array.include?(item)}.compact
+	find.size > 0 ? find[0] : nil
 end
