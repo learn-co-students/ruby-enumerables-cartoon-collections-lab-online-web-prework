648d2d57bad486b605fa6d3e6561001ac1f2fc03
Dan Foley <dan@dan-foley.com>
2019-12-18 22:55:30 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ end
 def find_the_cheese(arr)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  p arr.find {|n| if n == cheese_types.include?(n)}
+  arr.find {|n| if n == cheese_types.include?(n)}
 end
 
 contains_gouda = ["potato", "gouda", "camembert"]
