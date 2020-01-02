88ebc3d69c0cce4730038357b313a6c082188d19
legacy-gopher-9954 <fabianjohnson07@gmail.com>
2020-01-02 18:17:13 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,27 @@
 def roll_call_dwarves# code an argument here
   # Your code here
+  roll_call_dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"] {
+  puts roll_call_dwarves.each_with index}
 end
 
 def summon_captain_planet# code an argument here
   # Your code here
+  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+  puts planeteer_calls.collect
+end
+  puts planeteer_calls.capitalize
 end
 
 def long_planeteer_calls# code an argument here
   # Your code here
+  short_words = ["puff", "go", "two"]
+  puts longest.sort!{|a, b| a.length <=> b.length} 
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks = ["crackers", "gouda", "thyme"]
+  soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
+  puts find_the_cheese.include |cheese_types|
 end
